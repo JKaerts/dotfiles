b2dbaf1eeb3dd15353fefecf67643f700beb7607
Jonas Kaerts <jonaskaerts@gmail.com>
2019-03-31 17:23:40 +0200

    Added install script for racket packages

---
+++ install-racket-packages-windows.bat
@@ -0,0 +1,11 @@
+REM The helper code for Beautiful Racket
+raco pkg install beautiful-racket
+
+REM HTML parsing and writing
+raco pkg install html-parsing html-writing
+
+REM Pollen and the hyphenation library
+raco pkg install pollen pollen-tfl hyphenate
+
+REM The SICP helper language
+raco pkg install sicp
