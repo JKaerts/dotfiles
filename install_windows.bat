ae758b1d8d655d5bfe690d9b68347e534d810820
Jonas Kaerts <jonaskaerts@gmail.com>
2019-04-16 09:29:04 +0200

    made universal windows install script

--- install_windows.bat
+++ install_windows.bat
@@ -3,4 +3,4 @@ mklink /H %HOME%\.bashrc bash\.bashrc
 mklink /H %HOME%\.bash_profile bash\.bash_profile
 
 REM Git
-mklink /H %HOME%\.gitconfig\.gitconfig
+mklink /H %HOME%\.gitconfig git\.gitconfig
