ccb31e71f832fc40935d364ad6a190d37acb5d53
Jonas Kaerts <u0078549@kuleuven.be>
2019-04-25 13:41:50 +0200

    changed racket config from batch file to shell script

--- racket/configure-racket-windows.sh
+++ racket/configure-racket-windows.sh
@@ -1,11 +1,11 @@
-REM The helper code for Beautiful Racket
+# The helper code for Beautiful Racket
 raco pkg install beautiful-racket
 
-REM HTML parsing and writing
+# HTML parsing and writing
 raco pkg install html-parsing html-writing
 
-REM Pollen and the hyphenation library
+# Pollen and the hyphenation library
 raco pkg install pollen pollen-tfl hyphenate
 
-REM The SICP helper language
+# The SICP helper language
 raco pkg install sicp
