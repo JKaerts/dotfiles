dfffa0664e33532cd266dc3ffed61632d67fe187
Jonas Kaerts <jonas.kaerts@kuleuven.be>
2020-09-14 15:19:15 +0200

    move emacs deployment into function

--- deploy_dotfiles.sh
+++ deploy_dotfiles.sh
@@ -1,7 +1,13 @@
-for file in 'init.el' 'default-changes.el';
-do
-	if [ -f ~/.emacs.d/$file ]; then
-		rm $HOME/.emacs.d/$file;
-	fi;
-	cp ./emacs/$file ~/.emacs.d/$file
-done;
+#!/bin/bash
+
+deploy_emacs() {
+	for file in 'init.el' 'default-changes.el';
+	do
+		if [ -f ~/.emacs.d/$file ]; then
+			rm $HOME/.emacs.d/$file;
+		fi;
+		cp ./emacs/$file ~/.emacs.d/$file
+	done;
+}
+
+deploy_emacs
