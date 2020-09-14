de7f160b227b867583e43b22aa0878ecab81cb56
Jonas Kaerts <jonas.kaerts@kuleuven.be>
2020-09-14 15:25:26 +0200

    replace HOME variable with tilde

--- deploy_dotfiles.sh
+++ deploy_dotfiles.sh
@@ -14,7 +14,7 @@ deploy_emacs() {
 	for file in 'init.el' 'default-changes.el';
 	do
 		if [ -f ~/.emacs.d/$file ]; then
-			rm $HOME/.emacs.d/$file;
+			rm ~/.emacs.d/$file;
 		fi;
 		cp ./emacs/$file ~/.emacs.d/$file
 	done;
